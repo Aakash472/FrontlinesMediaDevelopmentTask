b96780a8f081f899850ae14611a00cfa5da77ec2
Aakash Patel <aakashpatel73068@gmail.com>
2025-11-10 23:54:22 +0530

    Task Completed

--- FrontlinesMediaFrontendDevelopmentTask/src/Components/APIFetchingData.jsx
+++ FrontlinesMediaFrontendDevelopmentTask/src/Components/APIFetchingData.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const fetchCompanies = async () => {
   try {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    const response = await axios.get('http://localhost:4972/findAll')
+    const response = await axios.get(`${import.meta.env.VITE_URL}/findAll`)
     return { success: true, data: response.data.data };
   } catch (error) {
     return { success: false, message: response.data.message };
